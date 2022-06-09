4d28b0f70532506fb69541eb1fc9f26e15db0d28
Hritwik <hritwiks.int@media.net>
2022-06-09 19:22:56 +0530

    Temp: code cleanup

--- sample/src/main/java/com/microsoft/gctoolkit/sample/Main.java
+++ sample/src/main/java/com/microsoft/gctoolkit/sample/Main.java
@@ -1,34 +1,36 @@
 package com.microsoft.gctoolkit.sample;
 
 import com.microsoft.gctoolkit.GCToolKit;
+import com.microsoft.gctoolkit.event.GarbageCollectionTypes;
 import com.microsoft.gctoolkit.io.GCLogFile;
 import com.microsoft.gctoolkit.io.SingleGCLogFile;
 import com.microsoft.gctoolkit.jvm.JavaVirtualMachine;
 import com.microsoft.gctoolkit.sample.aggregation.CollectionCycleCountsSummary;
 import com.microsoft.gctoolkit.sample.aggregation.HeapOccupancyAfterCollectionSummary;
 import com.microsoft.gctoolkit.sample.aggregation.PauseTimeSummary;
+import com.microsoft.gctoolkit.sample.collections.XYDataSet;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.Optional;
 
 public class Main {
+    private int initialMarkCount = 0;
+    private int remarkCount = 0;
+    private int defNewCount = 0;
 
-    public static void main(String[] args) throws IOException {
-        String userInput = args.length > 0 ? args[0] : "";
-        String gcLogFile = System.getProperty("gcLogFile", userInput);
+    public int getInitialMarkCount() {
+        return initialMarkCount;
+    }
 
-        if (gcLogFile.isBlank()) {
-            throw new IllegalArgumentException("This sample requires a path to a GC log file.");
-        }
+    public int getRemarkCount() {
+        return remarkCount;
+    }
 
-        if (Files.notExists(Path.of(gcLogFile))) {
-            throw new IllegalArgumentException(String.format("File %s not found.", gcLogFile));
-        }
-       
-        Main main = new Main();
-        main.analyze(gcLogFile);
+    public int getDefNewCount() {
+        return defNewCount;
     }
 
     public void analyze(String gcLogFile) throws IOException {
@@ -53,29 +55,30 @@ public class Main {
         JavaVirtualMachine machine = gcToolKit.analyze(logFile);
 
         // Retrieves the Aggregation for HeapOccupancyAfterCollectionSummary. This is a time-series aggregation.
-        String message = "The XYDataSet for %s contains %s items.\n\n";
+        String message = "The XYDataSet for %s contains %s items.\n";
 
-        machine.getAggregation(HeapOccupancyAfterCollectionSummary.class)
-                .map(HeapOccupancyAfterCollectionSummary::get)
-                .ifPresent(summary -> {
-                    summary.forEach((gcType, dataSet) -> {
-                        System.out.printf(message, gcType, dataSet.size());
-                        switch (gcType) {
-                            case DefNew:
-                                defNewCount = dataSet.size();
-                                break;
-                            case InitialMark:
-                                initialMarkCount = dataSet.size();
-                                break;
-                            case Remark:
-                                remarkCount = dataSet.size();
-                                break;
-                            default:
-                                System.out.println(gcType + " not managed");
-                                break;
-                        }
-                    });
-                });
+        Optional<HeapOccupancyAfterCollectionSummary> temp = machine.getAggregation(HeapOccupancyAfterCollectionSummary.class);
+        temp.map(HeapOccupancyAfterCollectionSummary::get).ifPresent(summary -> {
+            for (Map.Entry<GarbageCollectionTypes, XYDataSet> entry : summary.entrySet()) {
+                GarbageCollectionTypes gcType = entry.getKey();
+                XYDataSet dataSet = entry.getValue();
+                System.out.printf(message, gcType, dataSet.size());
+                switch (gcType) {
+                    case DefNew:
+                        defNewCount = dataSet.size();
+                        break;
+                    case InitialMark:
+                        initialMarkCount = dataSet.size();
+                        break;
+                    case Remark:
+                        remarkCount = dataSet.size();
+                        break;
+                    default:
+                        System.out.println(gcType + " not managed");
+                        break;
+                }
+            }
+        });
 
         Optional<CollectionCycleCountsSummary> summary = machine.getAggregation(CollectionCycleCountsSummary.class);
         summary.ifPresent(s -> s.printOn(System.out));
@@ -84,25 +87,26 @@ public class Main {
             System.out.println();
             System.out.printf("Total pause time                  : %.2f sec\n", pauseTimeSummary.getTotalPauseTime());
             System.out.printf("Total run time for the program    : %.2f sec\n", pauseTimeSummary.getRuntimeDuration());
-            System.out.printf("Percent pause time                : %.2f %%\n", pauseTimeSummary.getPercentPaused());
+            System.out.printf("Percent pause time                : %.3f %%\n", pauseTimeSummary.getPercentPaused());
             System.out.printf("Percent Throughput                : %.4f %%\n", pauseTimeSummary.getThroughput());
         });
 
     }
 
-    private int initialMarkCount = 0;
-    private int remarkCount = 0;
-    private int defNewCount = 0;
+    public static void main(String[] args) throws IOException {
+        String userInput = args.length > 0 ? args[0] : "";
+        String gcLogFile = System.getProperty("gcLogFile", userInput);
 
-    public int getInitialMarkCount() {
-        return initialMarkCount;
+        if (gcLogFile.isBlank()) {
+            throw new IllegalArgumentException("This sample requires a path to a GC log file.");
+        }
+
+        if (Files.notExists(Path.of(gcLogFile))) {
+            throw new IllegalArgumentException(String.format("File %s not found.", gcLogFile));
+        }
+
+        Main main = new Main();
+        main.analyze(gcLogFile);
     }
 
-    public int getRemarkCount() {
-        return remarkCount;
-    }
-
-    public int getDefNewCount() {
-        return defNewCount;
-    }
 }
